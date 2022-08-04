f439f37da299823fa76f4873017a9fa8b35524de
windi <windi.azmy@gmail.com>
2022-08-04 22:22:25 +0700

    tambah

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,8 +8,12 @@ import { useMealsDataQuery } from "../services/dataMeals";
 const Section = () => {
         
       const { data, error, isLoading } = useMealsDataQuery();
-
-      const loadData = isLoading ? <>loading</> : data.meals.map(item => <CardListSearch key={item.idMeal} item={item} />);
+      
+      let dataMeal;
+      if(data){
+        dataMeal = data.meals.slice(1,10);
+      }
+      const loadData = isLoading ? <>loading</> : dataMeal.map(item => <CardListSearch key={item.idMeal} item={item} />);
 
     return (
       <div className="flex flex-col md:p-4 w-5/6 flex-wrap gap-8 container mx-auto text-gray-600 ">
